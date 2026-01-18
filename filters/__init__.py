ac5e4012ba1cd2a82d844c9f1e955be06b957e23
z3dentalclinic-svg <z3dentalclinic@gmail.com>
2026-01-18 22:35:22 +0000

    Update __init__.py

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,12 +1,20 @@
 """
 Filters module for FGS Parser
+
+Contains all filtering logic:
+- batch_post_filter: Main geo-filter with multi-layer checks
+- entity_logic: Morphology and NER filtering (DEPRECATED)
+- infix_filter: Infix results filtering
+- relevance_filter: Relevance keyword filtering
 """
 
 from .batch_post_filter import BatchPostFilter, DISTRICTS_EXTENDED
+from .infix_filter import filter_infix_results
+from .relevance_filter import filter_relevant_keywords
 
 __all__ = [
     'BatchPostFilter',
-    'DISTRICTS_EXTENDED'
+    'DISTRICTS_EXTENDED',
+    'filter_infix_results',
+    'filter_relevant_keywords'
 ]
-
-
