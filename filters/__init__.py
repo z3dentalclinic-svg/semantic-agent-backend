a7960ded5bfae6b73ff9ddf5a71f553f56ef36fe
z3dentalclinic-svg <z3dentalclinic@gmail.com>
2026-01-18 13:15:27 +0000

    Create __init__.py

---
+++ filters/__init__.py
@@ -0,0 +1,10 @@
+python"""
+Filters module for FGS Parser
+"""
+
+from .batch_post_filter import BatchPostFilter, DISTRICTS_EXTENDED
+
+__all__ = [
+    'BatchPostFilter',
+    'DISTRICTS_EXTENDED'
+]
