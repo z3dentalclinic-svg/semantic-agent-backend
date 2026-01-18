20e9e31ed1119c906e1a61fd27a8cb58b863123b
z3dentalclinic-svg <z3dentalclinic@gmail.com>
2026-01-18 19:20:15 +0000

    Delete filters/geo directory

--- filters/geo/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Geo module for FGS Parser
-Handles geo blacklist generation and city database loading
-"""
-
-from .blacklist import generate_geo_blacklist_full
-
-__all__ = ['generate_geo_blacklist_full']
