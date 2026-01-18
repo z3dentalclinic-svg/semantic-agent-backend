944acc64f2d671173be838437e5345786ecb8f59
z3dentalclinic-svg <z3dentalclinic@gmail.com>
2026-01-18 19:45:29 +0000

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,14 @@
+"""
+Configuration module for FGS Parser
+"""
+
+from .constants import USER_AGENTS, WHITELIST_TOKENS, MANUAL_RARE_CITIES
+from .forbidden_geo import FORBIDDEN_GEO
+
+__all__ = [
+    'USER_AGENTS',
+    'WHITELIST_TOKENS', 
+    'MANUAL_RARE_CITIES',
+    'FORBIDDEN_GEO'
+]
 
