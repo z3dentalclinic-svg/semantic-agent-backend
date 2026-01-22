e76ca386eab26e2ac504fc33790aaf96a5827687
z3dentalclinic-svg <z3dentalclinic@gmail.com>
2026-01-22 01:10:03 +0000

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,4 @@
-"""
-Utils module for FGS Parser
-Contains utility functions and helpers
-"""
-
+# utils package
 from .normalizer import normalize_keywords
 
-__all__ = [
-    'normalize_keywords'
-]
+__all__ = ['normalize_keywords']
