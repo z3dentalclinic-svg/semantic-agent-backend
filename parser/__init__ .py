f48bdd176a181da83f868af5d093b509675252bf
z3dentalclinic-svg <z3dentalclinic@gmail.com>
2026-03-15 19:28:45 +0000

    Update __init__ .py

--- parser/__init__ .py
+++ parser/__init__ .py
@@ -2,3 +2,5 @@ from parser.suffix_generator import SuffixGenerator
 from parser.suffix_parser import SuffixParser
 from parser.prefix_generator import PrefixGenerator
 from parser.prefix_parser import PrefixParser
+from parser.infix_generator import InfixGenerator
+from parser.infix_parser import InfixParser
